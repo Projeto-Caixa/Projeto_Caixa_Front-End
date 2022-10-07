05ef4dcccb33019ed737ada6a67d55c57c1ba16b
Maurício Júnior <456junior456@gmail.com>
2022-10-06 22:35:09 -0300

    Feat: f

--- src/Components/ReportGlobal/index.jsx
+++ src/Components/ReportGlobal/index.jsx
@@ -3,11 +3,12 @@ import { ReportService } from "../../Services/reportService";
 import * as S from "./style";
 
 const ReportGlobal = () => {
-  const [list, setList] = useState([[]]);
+  const [listSales, setListSales] = useState();
 
   const renderLog = async () => {
     const response = await ReportService.Get();
-    setList([response.data]);
+    setListSales(response.data);
+    // console.log(listSales);
   };
 
   useEffect(() => {
@@ -26,18 +27,28 @@ const ReportGlobal = () => {
         <div>Valor</div>
       </S.Infos>
       <S.Products>
-        {list[0].map((element, index) => {
-          let disconvert = JSON.parse(element.list);
-          return (
-            <div key={index} className="products">
-              <S.ProductsContainer>
-                <span>{disconvert.name}</span>
-                <span>{disconvert.quantity}</span>
-                <span>{disconvert.price * disconvert.quantity}</span>
-              </S.ProductsContainer>
-            </div>
-          );
-        })}
+        {listSales
+          ? listSales.map((element, index) => {
+              let disconvert = JSON.parse(element.list);
+
+              // console.log(index, "☮🕳🕳☮✝✝☪☪🕉🕉♈🛐🛐", disconvert);
+
+              return disconvert.map((index) => {
+                // console.log("qqqqqqqqqqqqqqqqqqqq");
+                // console.log(index.name);
+
+                return (
+                  <div key={index} className="products">
+                    <S.ProductsContainer>
+                      <span>{index.name}</span>
+                      <span>{index.quantity}</span>
+                      <span>{index.price * index.quantity}</span>
+                    </S.ProductsContainer>
+                  </div>
+                );
+              });
+            })
+          : console.log("f")}
       </S.Products>
       <S.Dice>
         <div>Emitente: Mauricio</div>
