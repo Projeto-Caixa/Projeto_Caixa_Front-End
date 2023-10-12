6dd83e5ce47e68c4b4a04cbf0fe6fdcffea17433
Maurício Júnior <456junior456@gmail.com>
2023-10-12 07:46:27 -0300

    fix: url

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import HeadderCastem from '../../components/Headder';
-import * as S from './style';
-import { toast } from 'react-toastify';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import HeadderCastem from "../../components/Headder";
+import * as S from "./style";
+import { toast } from "react-toastify";
 
 const Home = () => {
   const navigate = useNavigate();
+  let isAdmin = localStorage.getItem("isAdmin");
 
   /// is logged
 
   useEffect(() => {
     isLogged();
+    verify();
   }, []);
   let isLogged = () => {
-    let jwt = localStorage.getItem('jwt');
+    let jwt = localStorage.getItem("jwt");
     if (!jwt) {
-      navigate('/logar');
-      toast('realize login novamente', {
-        icon: '🔄',
+      navigate("/logar");
+      toast("realize login novamente", {
+        icon: "🔄",
       });
     }
+  };
+
+  let verify = () => {
+    if (isAdmin === "true") {
+      return (isAdmin = isAdmin);
+    } else isAdmin = null;
   };
   ///
 
@@ -28,19 +36,23 @@ const Home = () => {
     <S.Page>
       <HeadderCastem props="home" />
       <S.ButtonsLine>
-        <button id="sale" onClick={() => navigate('/vender')}>
+        <button id="sale" onClick={() => navigate("/vender")}>
           vender
         </button>
         {/* <button id="sale" onClick={() => navigate("/personal-log")}>
           pessoal
         </button> */}
-        <button id="products" onClick={() => navigate('/produtos')}>
+        <button id="products" onClick={() => navigate("/produtos")}>
           produtos
         </button>
-        <button id="log" onClick={() => navigate('/relatorio')}>
-          relatorio
-        </button>
-        <button id="log" onClick={() => navigate('/relatorio/byId')}>
+        {isAdmin === "true" ? (
+          <button id="log" onClick={() => navigate("/relatorio")}>
+            relatorio
+          </button>
+        ) : (
+          ""
+        )}
+        <button id="log" onClick={() => navigate("/relatorio/byId")}>
           relatorioPessoal
         </button>
       </S.ButtonsLine>
