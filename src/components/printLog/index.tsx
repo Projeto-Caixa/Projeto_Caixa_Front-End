c0fffc6ff087a29f66cc29cbc14e62812cd6ab3a
Maurício Júnior <456junior456@gmail.com>
2024-02-17 15:56:51 -0300

    Fix: renames replaces

--- src/components/printLog/index.tsx
+++ src/components/printLog/index.tsx
@@ -70,7 +70,7 @@ const PrintLog = (props: any) => {
       </S.Log>
       <S.Total>Total: R$ {convert.format(totalList)}</S.Total>
       <S.SecundaryInfos>
-        <div>Baile</div>
+        <div>Almoço Pe Luis Carlos</div>
         <div>{today.toLocaleDateString()}</div>
       </S.SecundaryInfos>
     </S.PrintContainer>
