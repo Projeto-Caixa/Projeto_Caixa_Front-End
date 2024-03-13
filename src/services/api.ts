c743040898e54099095b1f78296a25d573cd3d8f
Maurício Júnior <456junior456@gmail.com>
2024-03-12 21:01:43 -0300

    fix: update api link url connection

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Api = axios.create({
   // baseURL: "https://projetocaixaback-end-production.up.railway.app",
-  baseURL: "https://projetocaixaback-end-production-04c1.up.railway.app",
+  baseURL: "castemapi.up.railway.app",
 });
 
 // Api.interceptors.request.use((config: any) => {
